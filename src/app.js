38a829b5d0b7552312df6b9e8cc78bac19bfd3bf
unknown <krishnarao.amperayani@gmail.com>
2022-02-14 20:27:43 +0530

    first

--- src/app.js
+++ src/app.js
@@ -21,12 +21,16 @@ const PLAYERS = [
     "Slingo"
 ];
 
-// initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
+    let detailedPlayers = [];
+	players.map((player, i) => {
+		let character = {};
+		character.name = player;
+		character.strength = getRandomStrength();
+		character.image = `images/super-${i + 1}.png`;
+		character.type = getRandomStrength() > 50 ? "hero" : "villain";
+		detailedPlayers.push(character);
+	});
 
     return detailedPlayers;
 }
@@ -36,18 +40,24 @@ const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
-// Build player template
+
 const buildPlayers = (players, type) => {
     let fragment = '';
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
+    
+    fragment = players
+		.filter((player) => player.type.toLowerCase() == type.toLowerCase())
+		.map((player, i) => {
+			return `<div class="player">
+                <img src="${player.image}" alt="">
+                <div class="name">${player.name}</div>
+                <div class="strength">${player.strength}</div>
+            </div>`;
+		})
+		.join("");
+	return fragment;
+};
 
-    return fragment;
-}
-
-// Display players in HTML
 const viewPlayers = (players) => {
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
